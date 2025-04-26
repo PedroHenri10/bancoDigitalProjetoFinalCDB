6d11a4428fbae6c7429d7ecdf0cf0662f3c97a10
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-04-25 22:36:48 -0300

    Create TransferenciaRepository.java

---
+++ src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/repository/TransferenciaRepository.java
@@ -0,0 +1,13 @@
+package br.com.cdb.bancoDigitalProjetoFinalCDB.repository;
+
+import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.Transferencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
+
+    List<Transferencia> findByContaOrigemId(Long contaId);
+    List<Transferencia> findByContaDestinoId(Long contaId);
