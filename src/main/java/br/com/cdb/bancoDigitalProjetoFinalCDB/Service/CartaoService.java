dc0f9c4b1fe728e023ef10cca28624c29ecbe474
PedroHenri10 <dinhonoliver@gmail.com>
2025-04-30 16:57:25 -0300

    criarCartaoDebito metdo criado

--- src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/Service/CartaoService.java
+++ src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/Service/CartaoService.java
@@ -1,5 +1,19 @@
 package br.com.cdb.bancoDigitalProjetoFinalCDB.Service;
 
+import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.Cartao;
+import br.com.cdb.bancoDigitalProjetoFinalCDB.repository.CartaoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
 public class CartaoService {
-//testee123
+
+    @Autowired
+    private CartaoRepository cartaoRepository;
+
+    public Cartao criarCartaoDebito(Cartao cartao){
+        cartao.setTipo("DEBITO");
+        cartao.setStatus(true);
+        return cartaoRepository.save(cartao);
+    }
 }
