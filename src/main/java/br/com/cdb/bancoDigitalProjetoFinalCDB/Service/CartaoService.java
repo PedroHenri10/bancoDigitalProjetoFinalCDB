e0b4fee5a57331ca35f5b95026b579e82e63da39
PedroHenri10 <dinhonoliver@gmail.com>
2025-04-30 23:16:01 -0300

    metodos ativar e desativar cartao criados

--- src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/Service/CartaoService.java
+++ src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/Service/CartaoService.java
@@ -1,6 +1,9 @@
 package br.com.cdb.bancoDigitalProjetoFinalCDB.Service;
 
 import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.Cartao;
+import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.enums.StatusCartao;
+import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.enums.TipoCartao;
+import br.com.cdb.bancoDigitalProjetoFinalCDB.exception.CartaoNaoEncontradoException;
 import br.com.cdb.bancoDigitalProjetoFinalCDB.repository.CartaoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +15,27 @@ public class CartaoService {
     private CartaoRepository cartaoRepository;
 
     public Cartao criarCartaoDebito(Cartao cartao){
-        cartao.setTipo("DEBITO");
-        cartao.setStatus(true);
+        cartao.setTipo(TipoCartao.DEBITO);
+        cartao.setStatus(StatusCartao.ATIVO);
         return cartaoRepository.save(cartao);
     }
+
+    public Cartao criarCartaoCredito(Cartao cartao){
+        cartao.setTipo(TipoCartao.CREDITO);
+        cartao.setStatus(StatusCartao.ATIVO);
+        return cartaoRepository.save(cartao);
+    }
+
+    public Cartao ativarCartao(Long id){
+        Cartao cartao = cartaoRepository.findById(id).orElseThrow(() -> new CartaoNaoEncontradoException("Cartao não encontrado."));
+        cartao.setStatus(StatusCartao.ATIVO);
+        return cartaoRepository.save(cartao);
+    }
+
+    public Cartao desativarCartao(Long id){
+        Cartao cartao = cartaoRepository.findById(id).orElseThrow(() -> new CartaoNaoEncontradoException("Cartao não encontrado."));
+        cartao.setStatus(StatusCartao.INATIVO);
+        return cartaoRepository.save(cartao);
+    }
+
 }
