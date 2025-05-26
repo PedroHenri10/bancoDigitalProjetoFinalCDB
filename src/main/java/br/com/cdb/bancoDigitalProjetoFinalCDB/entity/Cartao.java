616832ece2357f2f8a3a04ca8cec564802726b4c
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-05-26 15:57:24 -0300

    Update Cartao.java

--- src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/entity/Cartao.java
+++ src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/entity/Cartao.java
@@ -6,14 +6,29 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+		  use = JsonTypeInfo.Id.NAME,
+		  include = JsonTypeInfo.As.PROPERTY,
+		  property = "tipoCartao"
+		)
+		@JsonSubTypes({
+		  @JsonSubTypes.Type(value = ContaCorrente.class, name = "credito"),
+		  @JsonSubTypes.Type(value = ContaPoupanca.class, name = "debito")
+		})
+
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "tipo_cartao", discriminatorType = DiscriminatorType.STRING)
 @Table(name = "cartao")
 public class Cartao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int numeroCartao;
+    private Long numeroCartao;
     private int senha;
     private boolean ativo;
     private double faturaAtual;
@@ -26,13 +41,19 @@ public class Cartao {
 
     @ManyToOne
     @JoinColumn(name = "numero_conta")
+    @JsonBackReference(value = "conta-cartoes")
     private Conta conta;
+    
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    @JsonBackReference(value = "cliente-cartoes")
+    private Cliente cliente;
 
-    public int getNumeroCartao() {
+    public Long getNumeroCartao() {
         return numeroCartao;
     }
 
-    public void setNumeroCartao(int numeroCartao) {
+    public void setNumeroCartao(Long numeroCartao) {
         this.numeroCartao = numeroCartao;
     }
 
@@ -79,6 +100,14 @@ public class Cartao {
     public double getFaturaAtual(){
         return faturaAtual;
     }
+    
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 
     public void setFaturaAtual(double faturaAtual){
         this.faturaAtual = faturaAtual;
@@ -86,13 +115,14 @@ public class Cartao {
 
     public Cartao() {}
 
-    public Cartao(int numeroCartao, int senha, boolean ativo, StatusCartao status, TipoCartao tipo, Conta conta, double faturaAtual) {
+    public Cartao(Long numeroCartao, int senha, boolean ativo, StatusCartao status, TipoCartao tipo, Conta conta, Cliente cliente, double faturaAtual) {
         this.numeroCartao = numeroCartao;
         this.senha = senha;
         this.ativo = ativo;
         this.status = status;
         this.tipo = tipo;
         this.conta = conta;
+        this.cliente = cliente;
         this.faturaAtual = faturaAtual;
     }
 
