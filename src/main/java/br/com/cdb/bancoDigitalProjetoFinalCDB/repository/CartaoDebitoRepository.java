95fc1edda11351073907f2a4c0df8d5d93c44a8e
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-07-13 22:56:37 -0300

    Delete src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/repository/CartaoDebitoRepository.java

--- src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/repository/CartaoDebitoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.cdb.bancoDigitalProjetoFinalCDB.repository;
-
-import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.CartaoDebito;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartaoDebitoRepository extends JpaRepository<CartaoDebito, Long> {
-}
