31896957f9dcffc9ceb54eaa3404fb46a108287c
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-07-21 23:00:13 -0300

    distinct para seguro le individualmente

--- src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/repository/CartaoRepository.java
+++ src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/repository/CartaoRepository.java
@@ -4,8 +4,11 @@ import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.Cartao;
 import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.Conta;
 import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.enums.TipoCartao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CartaoRepository extends JpaRepository<Cartao, Long> {
@@ -14,4 +17,11 @@ public interface CartaoRepository extends JpaRepository<Cartao, Long> {
     List<Cartao> findByConta_NumeroConta(Long numeroConta);
     List<Cartao> findAllByConta_NumeroConta(Long numeroConta);
     boolean existsByConta_NumeroContaAndTipo(Long numeroConta, TipoCartao tipoCartao);
+
+    @Query("SELECT DISTINCT c FROM Cartao c LEFT JOIN FETCH c.conta WHERE c.numeroCartao = :id")
+    Optional<Cartao> findByIdDistinct(@Param("id") Long id);
+
+    @Query("SELECT DISTINCT c FROM Cartao c WHERE c.cliente.id = :clienteId")
+    List<Cartao> findByClienteIdDistinct(@Param("clienteId") Long clienteId);
+
 }
