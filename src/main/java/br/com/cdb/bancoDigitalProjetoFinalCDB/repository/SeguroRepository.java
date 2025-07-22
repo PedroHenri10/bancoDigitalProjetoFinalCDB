30de0a5f3c2b62aad7953bd8f8656c7493545c20
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-07-21 23:01:33 -0300

    distinct para seguro

--- src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/repository/SeguroRepository.java
+++ src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/repository/SeguroRepository.java
@@ -1,17 +1,22 @@
 package br.com.cdb.bancoDigitalProjetoFinalCDB.repository;
 
+import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.CartaoCredito;
 import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.Seguro;
 import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.enums.TipoCartao;
 import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.enums.TiposSeguro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface SeguroRepository extends JpaRepository<Seguro, Long> {
-    List<Seguro> findByClienteId(Long clienteId);
-    List<Seguro> findByCartaoCredito_NumeroCartao(int numeroCartao);
 
-    boolean existsByCartaoCreditoAndTipo(TipoCartao cartao, TiposSeguro tipo);
+    @Query("SELECT DISTINCT s FROM Seguro s WHERE s.cartaoCredito IN :cartoesCredito")
+    List<Seguro> findByCartaoCreditoInDistinct(@Param("cartoesCredito") List<CartaoCredito> cartoesCredito);
+    List<Seguro> findByCartaoCredito_NumeroCartao(Long numeroCartao);
+
+    boolean existsByCartaoCreditoAndTipo(CartaoCredito cartaoCredito, TiposSeguro tipo);
 }
