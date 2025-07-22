2a372738785cd2b27d90a0a3bd32b2aab7685368
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-07-21 21:09:51 -0300

    listar cartao

--- src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/repository/CartaoRepository.java
+++ src/main/java/br/com/cdb/bancoDigitalProjetoFinalCDB/repository/CartaoRepository.java
@@ -1,15 +1,17 @@
 package br.com.cdb.bancoDigitalProjetoFinalCDB.repository;
 
 import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.Cartao;
+import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.Conta;
 import br.com.cdb.bancoDigitalProjetoFinalCDB.entity.enums.TipoCartao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CartaoRepository extends JpaRepository<Cartao, Long>{
-    List<Cartao> findByClienteId(Long clienteId);
-    List<Cartao> findByContaId(Long contaId);
-    Cartao findByNumeroConta(int numeroCartao);
-    boolean existsByContaIdAndTipoCartao(Long numeroConta, TipoCartao tipocartao);
+public interface CartaoRepository extends JpaRepository<Cartao, Long> {
+    List<Cartao> findByClienteId(Long clienteId);      
+    List<Cartao> findByConta(Conta conta);
+    List<Cartao> findByConta_NumeroConta(Long numeroConta);
+    List<Cartao> findAllByConta_NumeroConta(Long numeroConta);
+    boolean existsByConta_NumeroContaAndTipo(Long numeroConta, TipoCartao tipoCartao);
 }
